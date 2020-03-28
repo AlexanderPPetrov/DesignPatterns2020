a17157d643ca7c4d6f9065003be4b1ef2ba7eb65
Aleksander Petrov <apetrovdev@gmail.com>
2020-03-28 20:02:37 +0200

    Inital commit with the following patterns Observer Command Composite Visitor

---
+++ src/composite/Employee.java
@@ -0,0 +1,13 @@
+package composite;
+
+public interface Employee {
+	
+	public void add(Employee employee);
+	public void remove(Employee employee);
+	
+	public String getName();
+	public double getSalary();
+	
+	public void printData();
+	
+}
